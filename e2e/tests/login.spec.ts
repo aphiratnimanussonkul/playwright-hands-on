4c6ed1f675dbbe7a3c7aa55b7fa3ed900b06351a
Aphirat Nimanussonkul <b5923151@gmail.com>
2023-09-10 14:27:00 +0700

    [Tweet] refactor,
    by using before each

--- e2e/tests/login.spec.ts
+++ e2e/tests/login.spec.ts
@@ -3,29 +3,26 @@ import { LoginPage } from "../pages/login";
 import { HomePage } from "../pages/home";
 
 test.describe("User login Twittah!", () => {
-  test("User login with username and password successfully", async ({
-    page,
-  }) => {
-    const loginPage = new LoginPage(page);
-    const homePage = new HomePage(page);
+  let loginPage: LoginPage;
+  let homePage: HomePage;
+
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    homePage = new HomePage(page);
 
     await test.step("Visit Twittah!", async () => {
       await loginPage.visitTwittah();
     });
+  });
 
+  test("User login with username and password successfully", async () => {
     await test.step("Login with username and password", async () => {
       await loginPage.loginWithCorrectUsernamePassword();
       await homePage.expectToSeeHomePage();
     });
   });
 
-  test("User login with incorrect password", async ({ page }) => {
-    const loginPage = new LoginPage(page);
-
-    await test.step("Visit Twittah!", async () => {
-      await loginPage.visitTwittah();
-    });
-
+  test("User login with incorrect password", async () => {
     await test.step("Login with incorrect password", async () => {
       await loginPage.loginWithIncorrectPassword();
       await loginPage.expectToSeeErrorMessageLoginFailed();
